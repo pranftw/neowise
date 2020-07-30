aaad893880820c40c7685245c2f39117c21e4a02
pranavsastry <pranava.sri@gmail.com>
2020-07-30 19:59:22 +0530

    added setup.py

---
+++ neowise/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='neowise',
+      version='0.0.1',
+      description='A Deep Learning framework for beginners',
+      author='Pranav Sastry',
+      packages=['neowise'],
+      install_requires=['numpy'])
