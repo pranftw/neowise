3ea2a6487e954fa80ba49a8d90b7a835af2137ea
pranavsastry <pranava.sri@gmail.com>
2020-08-10 22:50:13 +0530

    typo in description changed

--- neowise/setup.py
+++ neowise/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="neowise",
       version='0.0.1',
-      description="A Deep Learning library for built from scratch using Python and NumPy",
+      description="A Deep Learning library built from scratch using Python and NumPy",
       author="Pranav Sastry",
       author_email="pranava.sri@gmail.com",
       url="https://github.com/pranavsastry/neowise",
