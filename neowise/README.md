60ca32115e0ed9401db15b4d2af6f4a1857c13e6
pranavsastry <pranava.sri@gmail.com>
2020-08-10 23:16:12 +0530

    added version 0.0.2

---
+++ neowise/README.md
@@ -0,0 +1,3 @@
+# neowise
+
+### A Deep Learning library built from scratch with Python and NumPy
--- neowise/setup.py
+++ neowise/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="neowise",
-      version='0.0.1',
+      version='0.0.2',
       description="A Deep Learning library built from scratch using Python and NumPy",
       author="Pranav Sastry",
       author_email="pranava.sri@gmail.com",
