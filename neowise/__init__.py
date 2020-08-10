f60f3b62e514ed8275315414b24c0c9c96ecb79a
pranavsastry <pranava.sri@gmail.com>
2020-08-10 22:04:10 +0530

    finished assembling files

--- neowise/__init__.py
+++ neowise/__init__.py
@@ -1,8 +1,9 @@
-from neowise.activations import *
-from neowise.cost_function import *
-from neowise.functional import *
-from neowise.layers import *
-from neowise.optimizers import *
-from neowise.plots import *
-from neowise.neural_net import *
-from neowise.regularizers import *
+import neowise.activations
+import neowise.cost_function
+import neowise.functional
+import neowise.layers
+import neowise.optimizers
+import neowise.plots
+import neowise.regularizers
+
+from neowise.neural_net import Model
