fa66ed6f6064a876be3a4719a15470e387128e2d
pranavsastry <pranava.sri@gmail.com>
2020-08-10 22:15:23 +0530

    made some changes

--- neowise/setup.py
+++ neowise/setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='neowise',
+setup(name="neowise-pranavsastry",
       version='0.0.1',
-      description='A Deep Learning library for beginners',
-      author='Pranav Sastry',
+      description="A Deep Learning library for built from scratch using Python and NumPy",
+      author="Pranav Sastry",
+      author_email="pranava.sri@gmail.com",
+      url="https://github.com/pranavsastry/neowise",
       packages=find_packages(),
       install_requires=['numpy', 'matplotlib', 'math', 'sklearn', 'pandas',
                         'seaborn', 'tensorflow', 'keras', 'hdfdict',
