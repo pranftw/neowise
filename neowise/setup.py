c415d4c4998c88a14231194edbe3993dd7f15c00
pranavsastry <pranava.sri@gmail.com>
2020-08-13 00:09:16 +0530

    released v0.0.6

--- neowise/setup.py
+++ neowise/setup.py
@@ -1,23 +1,19 @@
 from setuptools import setup, find_packages
 
-with open("LICENSE.md", "r") as fh:
-    license = fh.read()
 with open("DOCUMENTATION.md", "r") as fh:
     long_description = fh.read()
 setup(name="neowise",
-      version='0.0.5',
+      version='0.0.6',
       description="A Deep Learning library built from scratch using Python and NumPy",
       author="Pranav Sastry",
       author_email="pranava.sri@gmail.com",
-      license=license,
-      license_content_type="text/markdown",
       long_description=long_description,
-      long_description_content_type="text/markdown",
+      long_description_content_type='text/markdown',
       maintainer="Pranav Sastry",
       url="https://github.com/pranavsastry/neowise",
       packages=find_packages(),
       install_requires=['numpy', 'matplotlib', 'hdfdict', 'prettytable', 'tqdm'],
       classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License"
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License"
       ])
