2bce24812fb276f07bdbced605c28e0f865dd6eb
Mihir M Kestur <mihirkestur@outlook.com>
2020-08-14 10:25:45 +0530

    Added SoftSign activation function (corrected)

--- neowise/build/lib/neowise/activations.py
+++ neowise/build/lib/neowise/activations.py
@@ -111,5 +111,4 @@ class SoftSign:
     def forward(self):
         return self / (np.abs(self) + 1)
     def backward(self):
-        last_forward = np.abs(self) + 1 if self else SoftSign.forward(self)
-        return 1 / np.power(last_forward, 2)
+        return 1/(np.square(1 + np.abs(self)))
